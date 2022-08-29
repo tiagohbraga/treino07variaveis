1959eb13ef290f8de9567b0c534b59967b20d732
Tiago Braga <tiagohbraga@gmail.com>
2022-08-29 15:14:56 -0300

    Modificação no exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+/*let nome
+let idade
+console.log(typeof nome,typeof idade)
+
+não foi atribuido nenhum valor a variavel, por isso retornou undefined*/
+
+const nome = prompt ('Qual seu nome?')
+let idade = Number (prompt('Qual sua idade?'))
+console.log (`Olá ${nome}, você tem ${idade} anos.`)
